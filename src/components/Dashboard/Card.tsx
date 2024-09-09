d94a430ab0b81c0afdc8adbcffd4e0fc2cc2d3ce
Muhammad Zaryab <zaryab.110786@gmail.com>
2024-09-09 22:55:45 +0500

    Zaryab Dashboard CHangeing Almost Complete

---
+++ src/components/Dashboard/Card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AiOutlineFileSearch } from "react-icons/ai";
+
+
+const Card:React.FC = ()=>{
+    return (
+    <div className="bg-white flex flex-col justify-center items-center min-h-56 my-4 text-blackSecondary/55 rounded">
+        <AiOutlineFileSearch className="text-6xl"/>
+        <p className="">No Tests completed in the last 1 hour</p>
+    </div>)
+}
+
+
+export default Card ; 
